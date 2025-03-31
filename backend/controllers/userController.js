a298345b6f9cbb178edd8412870c682993a12c5f
samuel maveke <mavekepeter2022@gmail.com>
2025-03-31 21:45:44 +0300

    updated encryption algo

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import validator from 'validator'
-import bcrypt  from 'bcrypt'
+import bcryptjs  from 'bcryptjs'
 import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
 import {v2 as cloudinary} from 'cloudinary'
@@ -23,8 +23,8 @@ const registerUser = async (req,res) =>{
             return res.json({success:false,message:'enter strong password'})
         }
         //hashing user password
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password,salt)
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password,salt)
         const userData = {
             name,
             email,
@@ -48,7 +48,7 @@ const loginUser = async(req,res) =>{
         if (!user){
            return res.json({ success: false, message:"user does not exit" });
         }
-        const isMatch = await bcrypt.compare(password,user.password)
+        const isMatch = await bcryptjs.compare(password,user.password)
         if (isMatch) {
             const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
             res.json({success:true,token})
